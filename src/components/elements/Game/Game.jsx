a2a434b0e209ff378fa126f16c19bb52170eea38
Roman Humenyak <romangumenyak2003@gmail.com>
2023-05-11 23:46:01 +0300

    make script for answering questions

--- src/components/elements/Game/Game.jsx
+++ src/components/elements/Game/Game.jsx
@@ -1,24 +1,30 @@
 import React from "react"
 import style from "./Game.module.scss"
 
-const Game = () => {
+const Game = (props) => {
+
+    const {step, question, setVariant, length} = props;
+
+    const progresBarPercent = Math.round(step / length * 100); 
+
 
     return(
         <div className={style.game}>
             <div className={style.game__body}>
                 <div className={style.game__progress_bar}>
-                    <div className={style.game__progress_line}></div>
+                    <div className={style.game__progress_line} style={{"width": `${progresBarPercent}%`}}></div>
                 </div>
-                <img className={style.game__image} src="https://t3.ftcdn.net/jpg/03/45/97/36/360_F_345973621_sMifpCogXNoIDjmXlbLwx1QZA5ZmQVl8.jpg" alt="" />
-                <h2 className={style.game__title}>Answer №1</h2>
-                <p className={style.game__question}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eveniet doloribus at cupiditate quae!</p>
+                <div className={style.game__image}>
+                    <img src={question.image} alt="" />
+                </div>
+                <h2 className={style.game__title}>Question №{question.id}</h2>
+                <p className={style.game__question}>{question.title}</p>
             </div>
 
             <ul className={style.answers}>
-                <li className={style.answers__item}>Test text</li>
-                <li className={style.answers__item}>Test text</li>
-                <li className={style.answers__item}>Test text</li>
-                <li className={style.answers__item}>Test text</li>
+                {question.answers.map((el, index) => {
+                    return (<li key={index} onClick={() => {setVariant(index)}} className={style.answers__item}>{el}</li>)
+                })}
             </ul>
         </div>
     )
