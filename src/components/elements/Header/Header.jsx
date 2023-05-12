61ba645f056c2392d5a4ce6e569d427eb36e85b9
Roman Humenyak <romangumenyak2003@gmail.com>
2023-05-12 16:30:43 +0300

    exported the data to a separate file

--- src/components/elements/Header/Header.jsx
+++ src/components/elements/Header/Header.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import style from "./Header.module.scss"
 import logo from "../../../image/logo.svg"
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const burgerHandler = () => {
+        if(!isOpen){
+            setIsOpen(true);
+        } else {
+            setIsOpen(false);
+        }
+
+    }
+
     return (
         <header className={style.header}>
             <img src={logo} className={style.header__logo} alt="" />
             <h2 className={style.header__title}>Quiz game</h2>
 
-            <div className={style.burger}>
+           <div onClick={burgerHandler} className={`${style.burger} ${isOpen ? style.open : ""}`}>
                 <div className={style.burger__body}></div>
             </div>
         </header>   
@@ -16,23 +29,3 @@ const Header = () => {
 }
 
 export default Header;
-
-
-// const burger = document.querySelector('.burger'),
-    //   windowMenu = document.querySelector(".window");
-
-// let menuOpen = false;
-
-// burger.addEventListener('click', ()=> {
-//     if(!menuOpen) {
-//         burger.classList.add('open');
-//         menuOpen=true;
-//         windowMenu.style.display = "block";
-//         windowMenu.style.overflow = "block";
-//     }
-//     else {
-//         burger.classList.remove('open');
-//         menuOpen=false;
-//         windowMenu.style.display = "none";
-//     }
-// })
